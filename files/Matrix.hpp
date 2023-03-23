f9abd989b090f46c156d229ceb688651368b1809
neel2233 <117990551+neel2233@users.noreply.github.com>
2023-03-23 09:25:22 +0200

    Изменения

--- files/Matrix.hpp
+++ files/Matrix.hpp
@@ -7,25 +7,23 @@ class Matrix
 private:
 	int m_n;
 	int m_m;
-	TYPE&& m_matrix = nullptr;
+	TYPE** m_matrix = nullptr;
 public:
 	Matrix(int n = 1, int m = 1) : m_n(n), m_m(m)
 	{
-		m_matrix = new TYPE&[m_n];
+		m_matrix = new TYPE*[m_n];
 		for(int i = 0; i < m_n; i++)
-			for (int j = 0; j < m_m; j++)
-			{
-				m_matrix[i] = new TYPE[j];
-			}
+		{
+			m_matrix[i] = new TYPE[m_m];
+		}
 	}
-	Matrix(const Matrix& M)
+	Matrix(const Matrix& M) : m_n(M.m_n), m_m(M.m_m)
 	{
-		m_matrix = new TYPE&[M.m_n];
+		m_matrix = new TYPE*[M.m_n];
 		for (int i = 0; i < M.m_n; i++)
-			for (int j = 0; j < M.m_m; j++)
-			{
-				m_matrix[i] = new TYPE[j];
-			}
+		{
+			m_matrix[i] = new TYPE[M.m_m];
+		}
 		for (int i = 0; i < M.m_n; i++)
 			for (int j = 0; j < M.m_m; j++)
 			{
@@ -35,19 +33,22 @@ public:
 
 	void fill(int n, int m, TYPE c)
 	{
-		m_matrix[n][m] = c;
+		m_matrix[n - 1][m - 1] = c;
 	}
 
+	TYPE get(int n, int m)
+	{
+		return m_matrix[n - 1][m - 1];
+	}
 	Matrix& operator=(Matrix M)
 	{
 		std::swap(m_n, M.m_n);
 		std::swap(m_m, M.m_m);
-		m_matrix = realloc(m_matrix, sizeof(TYPE) * m_n);
+		m_matrix = static_cast<TYPE**>(realloc(m_matrix, sizeof(TYPE**) * m_n));
 		for (int i = 0; i < m_n; i++)
-			for (int j = 0; j < m_m; j++)
-			{
-				m_matrix[i] = new TYPE[j];
-			}
+		{
+			m_matrix[i] = new TYPE[m_m];
+		}
 		return *this;
 	}
 	Matrix& operator+=(const Matrix& M)
@@ -65,9 +66,12 @@ public:
 	{
 		if (m_matrix != nullptr)
 		{
-			for (int i = 0; i < N; i++)
+			for (int i = 0; i < m_n; i++)
 			{
-				delete[] m_matrix[i];
+				if (m_matrix[i] != nullptr)
+				{
+					delete[] m_matrix[i];
+				}
 			}
 			delete[] m_matrix;
 		}
--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
-	Matrix<int> a(2, 2);
+	Matrix<int> A(2, 2);
+	Matrix<int> B(3, 2);
+	B.fill(1, 1, 3);
+	B.fill(1, 2, 6);
+	A = B;
+	std::cout << A.get(1, 1);
 	return 0;
 }
